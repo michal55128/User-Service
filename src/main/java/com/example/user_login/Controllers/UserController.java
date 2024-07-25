ba8d70d5a6e5e418c8885cda955a5ef386d82733
Simcha162 <151745632+Simcha162@users.noreply.github.com>
2024-07-25 15:51:47 +0300

    Update UserController.java

--- src/main/java/com/example/user_login/Controllers/UserController.java
+++ src/main/java/com/example/user_login/Controllers/UserController.java
@@ -5,6 +5,7 @@ import com.example.user_login.Entity.CustomUserDetails;
 import com.example.user_login.Services.UserService;
 import com.example.user_login.Entity.UserEntity;
 import com.example.user_login.Entity.UserLoginDTO;
+import com.example.user_login.Entity.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,150 +23,6 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserController
 {
-
-    //שינויים של שמחה
-
- //  @Autowired
- //  private final UserService userService;
- //  @Autowired
- //  private final JwtUtills jwtUtils;
- //  @Autowired
- //  private final  AuthenticationManager authenticationManager;
-
-
- //  @GetMapping("/")
- //  public String get() {
- //      return "hello";
- //  }
-
- //  @GetMapping()
- //  public List<UserEntity> getAllUsers() {
- //      return userService.getAllUsers();
- //  }
-
- //  @GetMapping("/{role}")
- //  public List<UserEntity> getUsersByRole(@PathVariable String role) {
- //      return userService.getUsersByRole(role);
- //  }
-
- //  @PostMapping("/{role}")
- //  public UserEntity addUser(@PathVariable String role, @RequestBody UserEntity user) {
- //      System.out.println("add user with role & email: " + role + user.getEmail());
- //      return userService.addUser(user.getEmail(), role);
- //  }
-
- //  @DeleteMapping("/{email}")
- //  public void removeUserPermission( @PathVariable String email) {
- //      System.out.println("change user permission with email:" + email );
- //      userService.removeUser(email);
- //  }
-
-
- //  @PostMapping("/login")
- //  public ResponseEntity<String> login(@RequestBody UserLoginDTO userLoginDTO) {
- //      System.out.println("Login attempt for user: " + userLoginDTO.getEmail());
- //       UserDetails userOpt = userService.loadUserByUsername(userLoginDTO.getEmail());
-
- //      if (userOpt != null) {
- //          if (userService.validatePassword(userLoginDTO.getPassword(), userOpt.getPassword())) {
-
- //              try {
- //                  authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userLoginDTO.getEmail(), userLoginDTO.getPassword()));
- //                  final UserDetails userDetails = userService.loadUserByUsername(userLoginDTO.getEmail());
- //                  String token = jwtUtils.generateToken(userDetails);
-
- //                  return ResponseEntity.ok(token);
- //              } catch (Exception e) {
- //                  return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
- //              }
- //          }
- //          else {
- //              return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
- //          }
- //      } else {
- //          return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
- //      }
-
- //  }
-
-
-
- //  @PostMapping("/register")
- //  public ResponseEntity<String> register(@RequestBody UserEntity user) {
- //      userService.saveUser(user);
- //      return ResponseEntity.ok("User registered successfully!");
- //  }
-
-
-//שינויים של מיכל
-
-//    private final UserService userService;
-//    private final JwtUtills jwtUtils;
-//    private final AuthenticationManager authenticationManager;
-//
-//    @GetMapping("/")
-//    public String get() {
-//        return "hello";
-//    }
-//
-//    @GetMapping("/users")
-//    public ResponseEntity<List<UserEntity>> getAllUsers() {
-//        return userService.getUsers();
-//    }
-//
-//    @GetMapping("/users/{role}")
-//    public List<UserEntity> getUsersByRole(@PathVariable String role) {
-//        return userService.getUsersByRole(role);
-//    }
-//    @GetMapping("/users/{id}")
-//    public UserEntity getUsersById(@PathVariable String id) {
-//        return userService.getUserById(id);
-//    }
-//
-//    @PostMapping("/users/{role}")
-//    public UserEntity addUser(@PathVariable String role, @RequestBody UserEntity user) {
-//        System.out.println("Adding user with role & email: " + role + " " + user.getEmail());
-//        return userService.addUser(user.getEmail(),role);
-//    }
-//
-//    @DeleteMapping("/users/{email}")
-//    public ResponseEntity<Void> removeUserPermission(@PathVariable String email) {
-//        System.out.println("Removing user with email: " + email);
-//        userService.deleteUser(email);
-//        return ResponseEntity.ok().build();
-//    }
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<String> login(@RequestBody UserLoginDTO userLoginDTO) {
-//        System.out.println("Login attempt for user: " + userLoginDTO.getEmail());
-//        UserDetails userDetails = userService.loadUserByUsername(userLoginDTO.getEmail());
-//
-//        if (userDetails != null) {
-//            if (userService.validatePassword(userLoginDTO.getPassword(), userDetails.getPassword())) {
-//                try {
-//                    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userLoginDTO.getEmail(), userLoginDTO.getPassword()));
-//                    final UserDetails authenticatedUser = userService.loadUserByUsername(userLoginDTO.getEmail());
-//                    String token = jwtUtils.generateToken(authenticatedUser);
-//                    return ResponseEntity.ok(token);
-//                } catch (Exception e) {
-//                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
-//                }
-//            } else {
-//                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
-//            }
-//        } else {
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
-//        }
-//    }
-//
-//    @PostMapping("/register")
-//    public ResponseEntity<String> register(@RequestBody UserEntity user) {
-//        userService.saveUser(user);
-//        return ResponseEntity.ok("User registered successfully!");
-//    }
-
-
-    //(הוספתי את הפונקציות: createUser, changeUserPermission, updateUser, deleteUser)שינויים של שמחה - המעודכן
     private final UserService userService;
     private final JwtUtills jwtUtils;
     private final AuthenticationManager authenticationManager;
@@ -181,7 +38,7 @@ public class UserController
     }
 
     @GetMapping("/users/{role}")
-    public List<UserEntity> getUsersByRole(@PathVariable String role) {
+    public List<UserEntity> getUsersByRole(@PathVariable Role role) {
         return userService.getUsersByRole(role);
     }
     @GetMapping("/users/{id}")
@@ -196,9 +53,9 @@ public class UserController
     }
 
     @PostMapping("/users/changeUserPermission")
-    public UserEntity changeUserPermission(@RequestParam String email, @RequestParam String role) {
-        System.out.println("Change user permission with email: " + email +" to role " +role);
-        return userService.changeUserPermission(email, role);
+    public UserEntity changeUserPermission(@RequestParam String email) {
+        System.out.println("Change user permission with email: " + email +" to role " +Role.USER);
+        return userService.changeUserPermission(email);
     }
 
     @PutMapping("/users/updateUser")
